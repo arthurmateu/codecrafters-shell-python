f4af843dd0e4316d6f420746d2425603d4359706
Arthur Mateu <arthurmateu@gmail.com>
2024-09-29 17:02:10 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 import sys
+import signal
 
 
 def main():
     sys.stdout.write("$ ")
     sys.stdout.flush()
-
+    
     command = input()
     print(f"{command}: command not found")
+    main()
 
 
 if __name__ == "__main__":
