b142d77323ab40a1c191269e9d81a13437eb78e8
admouse <arthurmateu@gmail.com>
2025-01-26 15:38:38 +0100

    few things

--- app/main.py
+++ app/main.py
@@ -1,13 +1,35 @@
 import sys
-import signal
 
+shell_builtins = ["echo", "exit", "type"]
+
+
+def echo(arguments):
+    print(' '.join(arguments))
+
+def type(cmd):
+    cmd = cmd[0]
+    if cmd in shell_builtins:
+        print(f"{cmd} is a shell builtin")
+    else:
+        print(f"{cmd}: not found")
 
 def main():
     sys.stdout.write("$ ")
     sys.stdout.flush()
-    
-    command = input()
-    print(f"{command}: command not found")
+
+    command = list(input().split())
+    main_command = command[0]
+    arguments = command[1:]
+
+    if main_command == "exit":
+        sys.exit(0)
+    elif main_command == "echo":
+        echo(arguments)
+    elif main_command == "type":
+        type(arguments)
+    else:
+        print(f"{main_command}: command not found")
+
     main()
 
 
