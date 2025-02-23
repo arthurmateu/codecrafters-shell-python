d082a091c7df06e2096d88a52c24fc84d5b0515d
admouse <arthurmateu@gmail.com>
2025-02-23 22:11:53 +0100

    codecrafters submit [skip ci]

--- app/funcs.py
+++ app/funcs.py
@@ -15,31 +15,6 @@ def exists_in_path(cmd):
             return path_location
 
 
-def clean_args(arguments):
-    if arguments[0] == "'" == arguments[-1] or arguments[0] == '"' == arguments[-1]:
-        N = len(arguments)
-
-        cleaned_arguments, i = [], 1
-        prev, prev_idx = arguments[0], 0
-
-        while i < N:
-            if arguments[i] == prev:
-                cleaned_arguments.append(arguments[prev_idx+1 : i])
-                i += 1
-
-                while i < N and arguments[i] == ' ':
-                    i += 1
-
-                if i < N:
-                    prev = arguments[i]
-                    prev_idx = i
-
-            i += 1
-        return cleaned_arguments
-
-    else:
-        return list(arguments.split())
-
 def echo(arguments):
     arguments = shlex.split(arguments)
 
@@ -52,7 +27,6 @@ def echo(arguments):
 
 
 def command_type(cmd):
-    cmd = cmd[0]
     if cmd in shell_builtins:
         print(f"{cmd} is a shell builtin")
 
@@ -64,7 +38,7 @@ def command_type(cmd):
 
 
 def run_command(cmd):
-    subprocess.run(cmd)
+    subprocess.run(shlex.split(cmd))
 
 
 def change_directory(dir):
